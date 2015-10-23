ed7d713893fbfda7deebbec7914065f07b02abb9
Mattias Cibien <mattias@mattiascibien.net>
2015-10-23 18:23:47 +0200

    Italian translaton

---
+++ locale/bootstrap-markdown.it.js
@@ -0,0 +1,24 @@
+﻿/**
+ * Italian translation for bootstrap-markdown
+ * by Mattias Cibien <mattias@mattiascibien.net>
+ */
+;(function($){
+  $.fn.markdown.messages['it'] = {
+    'Bold': "Grassetto",
+    'Italic': "Corsivo",
+    'Heading': "Título",
+    'URL/Link': "Inserisci un link",
+    'Image': "Inserisci un immagine",
+    'List': "Lista non ordinata",
+    'Preview': "Anteprima",
+    'strong text': "testo importante",
+    'emphasized text': "testo con enfasi",
+    'heading text': "titolo",
+    'enter link description here': "descrizione del link",
+    'Insert Hyperlink': "Inserte un collegamento ipertestuale",
+    'enter image description here': "descrizione dell'immagine",
+    'Insert Image Hyperlink': "Inserisci un'immagine con collegamento ipertestuale",
+    'enter image title here': "Inserire titolo dell'immagine",
+    'list text here': "Lista con testo"
+  };
+}(jQuery));
